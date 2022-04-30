d9cc20946a8d4696b38cbc384191387802258b6d
ANKAN <the.ankan17@gmail.com>
2022-04-30 23:40:46 +0530

    new changes

--- src/com/example/test/main.java
+++ src/com/example/test/main.java
@@ -4,9 +4,14 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Bank b=new User1();
-        System.out.println(b.createUser());
-        System.out.println(b.doTransaction(500));
+        Model m= new Model();
+        m.setId(11235813);
+        m.setName("Ankan");
+        m.setBalance(7000.00);
+        m.setPin(1724);
+        User user=new User(m);
+        user.credit();
+        
         
     }
 }
